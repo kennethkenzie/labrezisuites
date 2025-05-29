cb462139d8234258e4903bcf884253ab8872f8c4
kenneth <kennethkenzie48@gmail.com>
2025-05-29 20:16:03 +0300

    update

--- src/components/sections/footer.jsx
+++ src/components/sections/footer.jsx
@@ -19,7 +19,7 @@ const Footer = () => {
                         <div className="col-lg-6">
                             <div className="copyright-text extra-copyright">
                                 <p>
-                                    Crafted with ❤️ KenPro Media
+                                   
                                 </p>
                             </div>
                         </div>
