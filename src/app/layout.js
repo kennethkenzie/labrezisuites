570eff2388b447663d86953a8fd8ee13c8dfd9fc
kenneth <kennethkenzie48@gmail.com>
2025-05-29 22:21:26 +0300

    css2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,7 @@
-import Preloader from "@/components/ui/preloader";
 import "./global.css"
-import BootstrapForBrowser from "@/components/ui/bootstrapForBrowser";
+
 import Header from "@/components/sections/header";
-import CallToAction from "@/components/sections/callToAction";
-import Footer from "@/components/sections/footer";
+
 
 export const metadata = {
   title: "Robert Muyanja",
@@ -15,14 +13,14 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body suppressHydrationWarning>
         {/* <BootstrapForBrowser /> */}
-        <Preloader />
+       
         <Header />
         {children}
         {/* <CallToAction />
         <Footer /> */}
 
 
-        <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+        <script src="C:\Users\KEN\Documents\GitHub\labrezisuites\node_modules\flowbite\dist\flowbite.min.js"></script>
 
       </body>
     </html>
